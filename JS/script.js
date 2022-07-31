02ad6f57a4ef09081632d08723247fec14e50d5c
AminePro7 <108962666+AminePro7@users.noreply.github.com>
2022-07-31 17:18:52 +0100

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,13 @@
+//Calling all the fields 
+let title = document.getElementById('title');
+let price = document.getElementById('price');
+let taxes = document.getElementById('taxes');
+let ads = document.getElementById('ads');
+let discount = document.getElementById('discount');
+let total = document.getElementById('total');
+let count = document.getElementById('count');
+let category = document.getElementById('category');
+let create = document.getElementById('create');
+
+//We have to test them (if we have a wrong id)
+console.log(title, price, taxes, ads, discount, total, count, category);
